aaab4ae0ec7c8d14d13a8e8d591b214bd6a35e6c
Filipe Natanael <filipenatanael@live.com>
2018-11-02 23:37:16 -0300

    Feat: Create Sum method

---
+++ RestfulWebServicesCalculator/RestfulWebServicesCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestfulWebServicesCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        private int ConvertToDecimal(string secondNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
