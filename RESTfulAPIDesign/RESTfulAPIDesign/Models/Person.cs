85fb76a8b7b90b0bad292faba91978c4e193a286
Filipe Natanael <filipenatanael@live.com>
2018-11-03 23:02:46 -0300

    Feat: Create RESTful API Design
    Project to learn the verbs

---
+++ RESTfulAPIDesign/RESTfulAPIDesign/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulAPIDesign.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public long SecondName { get; set; }
+        public long Address { get; set; }
+        public long Gender { get; set; }
+    }
+}
