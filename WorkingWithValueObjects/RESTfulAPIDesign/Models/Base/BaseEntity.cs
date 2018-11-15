e68ba87d9311b5718aa02814a98b5060f564ba70
Filipe Natanael <filipenatanael@live.com>
2018-11-15 18:45:10 -0200

    Chore: Working With Value Objects

---
+++ WorkingWithValueObjects/RESTfulAPIDesign/Models/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace RESTfulAPIDesign.Models.Base
+{
+    /* Contract between attributes and table structure */
+    // [DataContract]
+    public class BaseEntity
+    {
+        [Column("id")]
+        public long? Id { get; set; }
+    }
+}
---
+++ WorkingWithValueObjects/RESTfulAPIDesign/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using RESTfulAPIDesign.Models;
+using RESTfulAPIDesign.Repository.Generic;
+
+namespace RESTfulAPIDesign.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private IRepository<Person> repository;
+
+        public PersonServiceImpl(IRepository<Person> repository)
+        {
+            this.repository = repository;
+        }
+        
+        public Person Create(Person person)
+        {   
+            // Validation here...
+            return this.repository.Create(person);
+        }
+
+        public List<Person> FindAll()
+        {
+            // Validation here...
+            return this.repository.FindAll();
+        }
+
+        public Person FindById(long id)
+        {
+            return this.repository.FindById(id);
+        }
+
+        public Person Update(Person person)
+        {
+            return this.repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public bool Exists(long id)
+        {
+            return this.repository.Exists(id);
+        }
+    }
+}
