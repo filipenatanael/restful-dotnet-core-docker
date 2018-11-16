8b36a7d3c230d17973ae76a9cfa49cc3cc6f6233
Filipe Natanael <filipenatanael@live.com>
2018-11-15 23:22:58 -0200

    Feat: Adding DataContract

--- DataContract/RESTfulAPIDesign/Data/ValuesObjects/PersonVO.cs
+++ DataContract/RESTfulAPIDesign/Data/ValuesObjects/PersonVO.cs
@@ -1,11 +1,23 @@
-﻿namespace RESTfulAPIDesign.Data.ValuesObjects
-{
+﻿using System.Runtime.Serialization;
+
+namespace RESTfulAPIDesign.Data.ValuesObjects
+{   
+    [DataContract]
     public class PersonVO
-    {
+    {   
+        [DataMember (Order = 1, Name ="code")]
         public long? Id { get; set; }
+
+        [DataMember(Order = 2)]
         public string FirstName { get; set; }
+
+        [DataMember(Order = 3)]
         public string LastName { get; set; }
+
+        [DataMember(Order = 5)]
         public string Address { get; set; }
+
+        [DataMember(Order = 4)]
         public string Gender { get; set; }
     }
 }
