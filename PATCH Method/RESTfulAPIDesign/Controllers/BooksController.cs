72c10db533ef5ec46e0abbfa50d39b30af72a7dd
Filipe Natanael <filipenatanael@live.com>
2018-11-17 15:38:07 -0200

    Feat: Implemented Patch Verbs

---
+++ PATCH Method/RESTfulAPIDesign/Controllers/BooksController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTfulAPIDesign.Data.ValuesObjects;
+using RESTfulAPIDesign.Models;
+using RESTfulAPIDesign.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tapioca.HATEOAS;
+
+namespace RESTfulAPIDesign.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class BooksController : Controller
+    {
+        private IBookService bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+        // GET api/version/controller
+        [HttpGet]
+        [ProducesResponseType(typeof(List<BookVO>), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(this.bookService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(BookVO), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(int id)
+        {
+            var book = this.bookService.FindById(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BookVO), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return new ObjectResult(this.bookService.Create(book));
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(List<BookVO>), 202)]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put(int id, [FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            var updateBook = this.bookService.Update(book);
+            if (updateBook == null) return BadRequest();
+            return new ObjectResult(updateBook);
+        }
+
+
+        [HttpPatch("{id}")]
+        [ProducesResponseType(typeof(List<BookVO>), 202)]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Patch(int id, [FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            var updateBook = this.bookService.Update(book);
+            if (updateBook == null) return BadRequest();
+            return new ObjectResult(updateBook);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Delete(int id)
+        {
+            this.bookService.Delete(id);
+            // Will return 204 status code
+            return NoContent();
+        }
+    }
+}
