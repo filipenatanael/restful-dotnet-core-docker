385223727de6edba771b48833a14d304d894b7ea
Filipe Natanael <filipenatanael@live.com>
2018-11-16 22:09:18 -0200

    Chore: added new migrations to users

--- Authentication/RESTfulAPIDesign/Models/Context/MySQLContext.cs
+++ Authentication/RESTfulAPIDesign/Models/Context/MySQLContext.cs
@@ -10,6 +10,6 @@ namespace RESTfulAPIDesign.Models.Context
 
         public DbSet<Person> Persons { get; set; }
         public DbSet<Book> Books { get; set;  }
-
+        public DbSet<User> Users { get; set; }
     }
 }
